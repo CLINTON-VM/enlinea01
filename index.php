b8badf27f2cf7fe3f3d2e1c96f87d50692050df7
CLINTON-VM <74558654@idexperujapon.edu.pe>
2024-10-05 12:07:40 -0500

    mi primer commit

--- index.php
+++ index.php
@@ -5,73 +5,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reutilizar</title>
-    <link rel="stylesheet" href="resources\css\bootstrap.min.css">
-    <link rel="stylesheet" href="./resources/css/bootstrap-icons/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="./resources/css/main.css">
+    <?php include 'partes/css.php'; ?>
 </head>
 
-<body class="px-3">
+<body class="">
     <!-- encabezado -->
-    <div class="row mt-5">
-        <div class="col-sm-12 col-md-6">
-            <h1 class="text-center">EN LÍNEA</h1>
-        </div>
-        <div class="col-sm-12 col-md-6">
-            <p class="text-right pr-5 ">SOLUCIONES MUNDIALES</p>
-        </div>
-    </div>
-    <!-- fin de encabezado -->
+    <?php include 'partes/header.php' ?>
 
-    <div class="row">
-        <div class="col-sm-12">
-            <img class="w-100" style="height: 350px;" src="resources/img/baner01.PNG" alt="">
-        </div>
-    </div>
     <div class="container mt-4 ">
         <h1 class="text-center">SOLUCIONES CREATIVAS</h1>
         <p>sin importar cual sea tu negocio, nuesto equipo puede hacerlo desde cero.</p>
 
-<div class="row">
-    <div class="col-ms-12 col-md-4">
-        <div class="card border-0">
-            <div class="card-body">
-                <img class="w-100" src="resources/img/idea_01.PNG" alt="">
-                <h5 class="h5">
-                    <i class="bi bi-pencil-square"></i> DISEÑO
-                </h5>
-                <P>Creamos diseños segun tus necesidades</P>
+        <div class="row">
+            <div class="col-ms-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img class="w-100" src="resources/img/idea_01.PNG" alt="">
+                        <h5 class="h5">
+                            <i class="bi bi-pencil-square"></i> DISEÑO
+                        </h5>
+                        <P>Creamos diseños segun tus necesidades</P>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
 
-    <div class="col-ms-12 col-md-4">
-        <div class="card border-0">
-            <div class="card-body">
-                <img class="w-100" src="resources/img/idea_02.PNG" alt="">
-                <h5 class="h5">
-                    <i class="bi bi-flower1"></i> DESARROLLO
-                </h5>
-                <P>Nuestros prohgramadores te garantizaran la perfeccion</P>
+            <div class="col-ms-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img class="w-100" src="resources/img/idea_02.PNG" alt="">
+                        <h5 class="h5">
+                            <i class="bi bi-flower1"></i> DESARROLLO
+                        </h5>
+                        <P>Nuestros prohgramadores te garantizaran la perfeccion</P>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
 
-    <div class="col-ms-12 col-md-4">
-        <div class="card border-0">
-            <div class="card-body">
-                <img class="w-100" src="resources/img/idea_03.PNG" alt="">
-                <h5 class="h5">
-                    <i class="bi bi-plus-square-fill"></i> EDICION
-                </h5>
-                <P>Que el producto final llegue a tus clientes</P>
+            <div class="col-ms-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img class="w-100" src="resources/img/idea_03.PNG" alt="">
+                        <h5 class="h5">
+                            <i class="bi bi-plus-square-fill"></i> EDICION
+                        </h5>
+                        <P>Que el producto final llegue a tus clientes</P>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <footer class="bg-vine">
-        <p class="p-5 text-white ">edificio IDEX PJ, Jr. Amazonas 120 - chachapoyas Chachapoyas -Amazonas. </p>
-    </footer>
+
+    <?php include 'partes/footer.php' ?>
 
 </body>
 
